16f5fe726d921e30912ff3679010abb4bd75b277
luks <luks@gluks.com>
2010-10-02 23:16:30 +0200

    fdr

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 
 # Uncomment below to force Rails into production mode
 # (Use only when you can't set environment variables through your web/app server)
- ENV['RAILS_ENV'] = 'production'
+# ENV['RAILS_ENV'] = 'production'
 
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
